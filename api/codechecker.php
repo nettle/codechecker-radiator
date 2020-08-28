a808c530d8fdfcb2e08be1f27c6bed4317c60c50
Alex <alexkrapivin@gmx.com>
2020-08-28 16:32:30 +0200

    Improve error handling in CodeChecker

--- api/codechecker.php
+++ api/codechecker.php
@@ -47,20 +47,28 @@ class CodeChecker
     {
         $jsonFileName = tempnam(sys_get_temp_dir(), "codechecker-result.json.");
         $command = $this->getCommand($command, $jsonFileName);
-        if ($this->debug)
-        {
+        if ($this->debug) {
             echo "Running: <b>$command</b><br>";
         }
-        $output = shell_exec($command);
+
+        // $output = shell_exec($command);
+        if (execute_shell_command($command, $stdout, $stderr)) {
+            $output = "COMMAND:\n" . $command . "\n\nSTDOUT:\n" . $stdout . "\n\nSTDERR:\n" . $stderr . "\n";
+        }
 
         // Read JSON from file
         $jsonfile = fopen($jsonFileName, "r") or die("Unable to open '$jsonFileName' file!");
-        $json = fread($jsonfile, filesize($jsonFileName));
+        $jsonsize = filesize($jsonFileName);
+        if ($jsonsize > 0) {
+            $json = fread($jsonfile, $jsonsize);
+        } else {
+            $json = "CodeChecker CLI error: failed to execute CodeChecker";
+            $json .= "\n......................\n" . $output;
+        }
         fclose($jsonfile);
         unlink($jsonFileName);
 
-        if ($this->debug)
-        {
+        if ($this->debug) {
             echo "Output:<pre>$output</pre>";
             echo "JSON ($jsonFileName):<pre>$json</pre>";
         }
@@ -99,4 +107,17 @@ class CodeChecker
     protected $path;
     protected $product;
     protected $serverUrl;
+}
+
+function execute_shell_command($cmd, &$stdout = null, &$stderr = null)
+{
+    $proc = proc_open($cmd, [
+        1 => ["pipe", "w"],
+        2 => ["pipe", "w"],
+    ], $pipes);
+    $stdout = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $stderr = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+    return proc_close($proc);
 }
